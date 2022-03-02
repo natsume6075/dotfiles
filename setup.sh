6cedc732bf393f677a338a9f66bdba4076397048
natsume6075 <natsume6075@gmail.com>
2022-03-02 11:08:31 +0900

    bashrc追加

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ echo $SCRIPT_DIR
 
 ln -sfv $SCRIPT_DIR/.gitconfig ~/.gitconfig
 ln -sfv $SCRIPT_DIR/.zshrc ~/.zshrc
+ln -sfv $SCRIPT_DIR/.bashrc ~/.bashrc
 ln -sfv $SCRIPT_DIR/.vimrc ~/.vimrc
 ln -sfv $SCRIPT_DIR/.tmux.conf ~/.tmux.conf
 
